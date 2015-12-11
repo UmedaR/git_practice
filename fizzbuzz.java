bf1c632e7c2bd2ef62f766e676cdf50995f6a81b
UmedaR <rumeda@crooz.co.jp>
2015-12-11 19:05:07 +0900

    initial commit

---
+++ fizzbuzz.java
@@ -0,0 +1,13 @@
+public class fizzbuzz{
+	public static void main(String[] args){
+		for(int i=1; i<=10; i++){
+			if(i % 3 == 0){
+			System.out.println("fezz");
+				}else if(i % 5 == 0){
+			System.out.println("buzz");
+				}else{
+					System.out.println(i);
+			}
+		}
+	}
+}
